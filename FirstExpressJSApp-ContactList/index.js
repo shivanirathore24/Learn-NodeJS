f0df88b670aca7c73114f679b31dded00c0c5fc2
shivanirathore24 <shivani.rathore2409@gmail.com>
2023-09-26 13:36:45 +0530

    Returning response from Server

--- FirstExpressJSApp-ContactList/index.js
+++ FirstExpressJSApp-ContactList/index.js
@@ -21,4 +21,14 @@ app.listen(port, function (err) {
   console.log("My Express server is running on port", port);
 });
 
-//localhost:8000 - Cannot GET / means I sent request but can't get page.
+
+//returning response from server
+app.get("/", function (req, res) {
+  res.send("<h1>Cool, it's running ! or is it? </h1>");
+});
+//localhost:8000 - Cool, it's running ! or is it?
+
+app.get("/profile", function (req, res) {
+  res.send("<h1>Getting Profile data ! </h1>");
+});
+//localhost:8000/profile - Getting Profile data !
