12cd91d3c3df792e6b0d9e613fa40fd55e2a32af
shivanirathore24 <shivani.rathore2409@gmail.com>
2023-10-01 18:09:36 +0530

    Parsing Form Data: Storing the Contact

--- FirstExpressJSApp-ContactList/index.js
+++ FirstExpressJSApp-ContactList/index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+//Praser takes the data from browser and creates body key inside request object, addes data inside body.
+app.use(express.urlencoded()); //signifies middleware
 
 contactList = [
   {
@@ -37,8 +39,27 @@ app.get("/practice", function (req, res) {
 
 //Controller : create-contact
 app.post("/create-contact", function (req, res) {
-  return res.redirect("/practice");
+  //as soon you kill the server, re-start the page so your Added Contact through form will get vanished becoz compiled file was
+  //saved in RAM andn will be removed from the memmory. Only hard-coded value will be there i.e SHivani, Neeraj, Shubman.
+  contactList.push(req.body);
+
+  return res.redirect("back"); //go back to page from which it was coming
 });
+
+/* OR
+app.post("/create-contact", function (req, res) {
+  console.log(req.body);
+  console.log(req.body.name);
+  console.log(req.body.phone);
+
+  contactList.push({
+    name: req.body.name,
+    phone: req.body.phone,
+  });
+
+  return res.redirect("/");
+});
+*/
 
 app.listen(port, function (err) {
   if (err) {
