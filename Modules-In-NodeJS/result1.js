f934c1745491f079878d86bf5ebbbc2280129791
shivanirathore24 <shivani.rathore2409@gmail.com>
2025-01-30 23:01:35 +0530

    Added: Removing mjs extension

---
+++ Modules-In-NodeJS/result1.js
@@ -0,0 +1,11 @@
+/*
+import * as arithmeticModule from "./arithmetic1.js";
+console.log(arithmeticModule.sum(10,20));
+console.log(arithmeticModule.num);
+*/
+
+import { sum } from "./arithmetic1.js";
+console.log(sum(10, 20));
+
+
+// Adding "type": "module"  in 'package.json' to remove .ejs extension to .js extension
--- Modules-In-NodeJS/result1.mjs
+++
@@ -1,8 +0,0 @@
-/*
-import * as arithmeticModule from "./arithmetic1.mjs";
-console.log(arithmeticModule.sum(10,20));
-console.log(arithmeticModule.num);
-*/
-
-import { sum } from "./arithmetic1.mjs";
-console.log(sum(10, 20));
