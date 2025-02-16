538deb8cbbe30e36ab1cd924790531dd1fe0e3fd
shivanirathore24 <shivani.rathore2409@gmail.com>
2025-02-16 23:03:59 +0530

    Added: Registeration Page

--- Inventory Management App/index.js
+++ Inventory Management App/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import ProductsController from "./src/controllers/product.controller.js";
+import UserController from "./src/controllers/user.controller.js";
 import ejsLayouts from "express-ejs-layouts";
 import path from "path";
 import validateRequest from "./src/middlewares/validation.middleware.js";
@@ -20,8 +21,11 @@ app.set("views", path.join(path.resolve(), "src", "views"));
 //Serves the static files from the views directory to the browser
 app.use(express.static("src/views"));
 
-//Create an instance of ProductController
+//Create an instance of ProductController & UserController
 const productsController = new ProductsController();
+const userController = new UserController();
+
+app.get("/register", userController.getRegister);
 app.get("/", productsController.getProducts);
 app.get("/new-product", productsController.getAddProduct);
 app.post(
